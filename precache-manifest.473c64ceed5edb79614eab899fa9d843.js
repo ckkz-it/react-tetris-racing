5c57d53021dc895647d643a4e3f8e7d43a114920
ckkz-it <cirkus.kz@gmail.com>
2020-03-21 12:18:48 +0300

    Updates

---
+++ precache-manifest.473c64ceed5edb79614eab899fa9d843.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d640a6818a7f911e9da84d02c82b26b8",
+    "url": "/react-tetris-racing/index.html"
+  },
+  {
+    "revision": "037e7d3c44f2eec7e443",
+    "url": "/react-tetris-racing/static/css/main.8cbb2d5a.chunk.css"
+  },
+  {
+    "revision": "cf1c0af1e9540a77c54c",
+    "url": "/react-tetris-racing/static/js/2.ac551e5e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-tetris-racing/static/js/2.ac551e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037e7d3c44f2eec7e443",
+    "url": "/react-tetris-racing/static/js/main.f5a69f94.chunk.js"
+  },
+  {
+    "revision": "6a8d1f8f9f3e1670dc1c",
+    "url": "/react-tetris-racing/static/js/runtime-main.bd3dbf98.js"
+  },
+  {
+    "revision": "635b18d042f84cbf256ce71b1bd0c98f",
+    "url": "/react-tetris-racing/static/media/bg.635b18d0.jpg"
+  },
+  {
+    "revision": "ec7fc8bd9ef63900b178c646cbc4c3fb",
+    "url": "/react-tetris-racing/static/media/tetris.ec7fc8bd.woff"
+  }
+]);
